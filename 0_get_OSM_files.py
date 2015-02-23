b2bbe523c264733da9404ba7d5d4d0070d56a392
chrisewing <ewingchris@gmail.com>
2015-02-23 21:04:21 +0000

    logging and country update
    - added Steve's updates for logging and calling Osmosis
    - also now all is based on geofabrik continent URLs (not on the Haiyan
    example)

--- 0_get_OSM_files.py
+++ 0_get_OSM_files.py
@@ -31,14 +31,18 @@ except ImportError:
     )
 
 
-def main(osm_url, workspace, get_all):
+def main(osm_url, workspace, get_all, country):
     """Get files form the given URL and put to the workspace if needed.
 
     :param str osm_url: url to PBF files (geofabrik)
     :param str workspace: local directory path for output
     :param str/bool get_all: a bool or bool-like string (1, from Arc)
+    :param str country: name of the country to be downloaded
 
     """
+    if osm_url[:-1] != r'/':
+        osm_url = osm_url + r'/'
+    
     def get_pbfs(url, fname):
         """Get PBF files from geofabrik."""
         sock = urllib2.urlopen(urlparse.urljoin(url, fname))
@@ -54,17 +58,20 @@ def main(osm_url, workspace, get_all):
     try:
         sock = urllib2.urlopen(osm_url)
         html_source = sock.read()
+        country = country.replace(" ","-")
+        country = country.lower()
         links = re.findall(
-            '\d{4}.\d{2}.\d{2}.\d{2}.\d{2}.osm.pbf"', html_source)
+            country + '-\d{6}.osm.pbf"', html_source)
         log_message("Found {} files.".format(len(links)))
         sock.close()
 
         if get_all and get_all in ["1", 1, True]:
-            for ix, link in enumerate(links[:1]):
+            for ix, link in enumerate(links):
                 _link = link[:-1]
-                debug("File number {} - {}".format(ix, link))
+                debug("File number {} - {}".format(ix, _link))
                 url = urlparse.urljoin(osm_url, _link)
-
+                #debug(url)
+                
                 if os.path.exists(os.path.join(workspace, _link)):
                     log_message("{} already exists".format(
                         os.path.join(workspace, _link)))
@@ -72,10 +79,11 @@ def main(osm_url, workspace, get_all):
 
                 log_message("Getting {}".format(url))
                 get_pbfs(osm_url, _link)
+                log_message("...downloaded")
 
         # get latest file too
         log_message("Getting latest.osm.pbf")
-        get_pbfs(osm_url, "latest.osm.pbf")
+        get_pbfs(osm_url, country + "-latest.osm.pbf")
 
         log_message("...finished downloading PBF files")
     except Exception as e:
@@ -99,13 +107,18 @@ if __name__ == '__main__':
         action='store_true',
         default=False
     )
-    parser.add_argument('osm_url')  # positional, rather than option (required)
+    # positional, rather than option (required)
+    parser.add_argument('osm_url')  
+    parser.add_argument('country')
 
     if ARC_ENABLED:
         osm_url = arcpy.GetParameterAsText(0)
-        workspace = arcpy.GetParameterAsText(1)
-        get_all = arcpy.GetParameterAsText(2)
-        main(osm_url, workspace, get_all)
+        country = arcpy.GetParameterAsText(1)
+        workspace = arcpy.GetParameterAsText(2)
+        get_all = arcpy.GetParameterAsText(3)
+        log_message(", ".join([osm_url,country,workspace,get_all]))
+
+        main(osm_url, workspace, get_all, country)
     else:
         args = parser.parse_args()
         if args.verbosity >= 2:
@@ -113,4 +126,4 @@ if __name__ == '__main__':
         else:
             logging.basicConfig(level=logging.INFO)
 
-        main(args.osm_url, args.workspace, args.get_all)
+        main(args.osm_url, args.workspace, args.get_all, args.country)
