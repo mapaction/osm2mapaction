087e1c07ed9bdc8b689e99d26cefd3327e8a51f0
Andy Smith <asmith@mapaction.org>
2015-02-24 12:43:33 +0000

    Updated travis config

--- osm2ma/tests.py
+++ osm2ma/tests.py
@@ -1,5 +1,6 @@
 
 import sys
+import test_raw_conf_loader
 # import test_configengine
 # import test_ogrwrapper
 
@@ -7,13 +8,15 @@ import sys
 def main():
     print "print sys.path:"
     print sys.path
-    # test_ogrwrapper.unittest.main()
-    # test_configengine.unittest.main()
     print ""
     print "importing gdal"
     from osgeo import gdal
     print "print gdal.__file__"
     print gdal.__file__
+
+    test_raw_conf_loader.unittest.main()
+    # test_configengine.unittest.main()
+    # test_ogrwrapper.unittest.main()
 
 if __name__ == '__main__':
     main()
